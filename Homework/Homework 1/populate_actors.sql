7103de9b7a0e9b3448bf9ca83ceec31367eb73c5
jglancy95 <jasmine.glancy.vt@gmail.com>
2025-01-08 20:48:05 -0500

    Refactor: Restructure query to remove NULL values

--- Homework/Homework 1/populate_actors.sql
+++ Homework/Homework 1/populate_actors.sql
@@ -17,7 +17,11 @@ WITH previous_year AS (
 			    SELECT generate_series(1970, 2021) AS year
 			),
 				people AS (
-				    SELECT actor, actorid, MIN(year) AS first_year
+				    SELECT 
+						actor,
+						actorid,
+						MIN(year) AS first_year,
+						MAX(year) AS last_year
 				    FROM actor_films
 				    GROUP BY actor, actorid
 				),
@@ -53,6 +57,7 @@ WITH previous_year AS (
 						            NULL
 						        ) AS films
 						    FROM actors_years ayd
+							-- Ensures all years from actors_years are included, even if there are no films that year
 						    LEFT JOIN actor_films a1
 						    ON ayd.actor = a1.actor AND ayd.year = a1.year
 						    LEFT JOIN people p
@@ -70,7 +75,7 @@ WITH previous_year AS (
 							    FROM actor_films
 							    GROUP BY actor
 							),
-								 -- Makes sure theres only one row per actor
+								 -- Makes sure there's only one row per actor
 								most_recent_actor_row AS (
 								    SELECT DISTINCT ON (wd.actor)
 								        wd.actor_id,
@@ -83,28 +88,12 @@ WITH previous_year AS (
 								    ON wd.actor = s.actor
 								    ORDER BY wd.actor, wd.year DESC
 								)
+								
 SELECT 
     mra.actor_id AS actor_id,
     mra.year AS current_year,
     mra.actor AS actor,
-    -- Populate the films 
-    CASE WHEN mra.films IS NULL
-        THEN ARRAY[ROW(
-            t.film,
-            t.year,
-            t.votes,
-            t.rating,
-            t.filmid
-        )::films]
-        WHEN mra.films IS NOT NULL THEN mra.films || ARRAY[ROW(
-            t.film,
-            t.year,
-            t.votes,
-            t.rating,
-            t.filmid
-        )::films]
-        ELSE mra.films
-    END as films,
+    mra.films AS films,
     -- make the quality class
     CASE
         WHEN (mra.films[CARDINALITY(mra.films)]).rating > 8 THEN 'star'
@@ -112,11 +101,19 @@ SELECT
         WHEN (mra.films[CARDINALITY(mra.films)]).rating > 6 THEN 'average'
         ELSE 'bad'
     END::quality_class,
-    (mra.films[CARDINALITY(mra.films)]).year = mra.year AS is_active
+	CASE 
+		-- If there is one item in the films array
+		WHEN CARDINALITY(mra.films) >= 1 
+			-- Check if the year for that film is equal to the current year 
+			THEN p.last_year = mra.year
+	END AS is_active
+	
 FROM this_year t
 FULL OUTER JOIN most_recent_actor_row mra 
 ON t.year = mra.year
-AND t.actor = mra.actor;
+LEFT JOIN people p
+ON p.actor = mra.actor; 
 
--- TODO: Remove NULL values
+-- TODO: Compare an actor's last film with their previous film to see if they are active
+
 -- TODO: Successfully populate the actors table
