d36695c0f505180cab9bfa20a24bcef3649ade47
jglancy95 <jasmine.glancy.vt@gmail.com>
2025-08-22 08:45:51 -0400

    add todos

---
+++ Homework/Homework 6/aggregation_job.py
@@ -0,0 +1,7 @@
+# TODO: Answer these questions:
+
+    # TODO: What is the average number of web events of a session 
+        # from a user on Tech Creator?
+    
+    # TODO: Compare results between different hosts 
+        # (zachwilson.techcreator.io, zachwilson.tech, lulu.techcreator.io)
